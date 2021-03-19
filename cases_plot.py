b161d04037b6441790de0e042969a2f237ab9386
bwestover <bwestover@github.com>
2021-03-18 17:40:16 -0700

    update code to work with new dataset

--- cases_plot.py
+++ cases_plot.py
@@ -5,32 +5,22 @@ from matplotlib import pyplot as plt
 
 COUNTY = sys.argv[1]
 
-with open('2019_ca_population_est.csv') as population_csv:
-    pop_csv_reader = csv.reader(population_csv)
-    # Data is 2019 estimate from U.S. census bureau - https://www2.census.gov/programs-surveys/popest/tables/2010-2019/counties/totals/co-est2019-annres-06.xlsx
-    county_population = {rows[0]:rows[1] for rows in pop_csv_reader}
-
-pop = int(county_population[COUNTY])
-
-#DEBUG
-#print(f"{COUNTY} population: {pop}")
-
-with open('statewide_cases.csv') as cases_csv:
+with open('covid19cases_test.csv') as cases_csv:
     cases_csv_reader = csv.reader(cases_csv)
-    # Data is from the California Open Data portal - https://data.ca.gov/dataset/covid-19-cases
+    # Data is from the California Health and Human Services portal - https://data.chhs.ca.gov/dataset/covid-19-time-series-metrics-by-county-and-state
     # Raw data headers
-    # county,totalcountconfirmed,totalcountdeaths,newcountconfirmed,newcountdeaths,date
-    
+    # date,area,area_type,population,cases,cumulative_cases,deaths,cumulative_deaths,total_tests,cumulative_total_tests,positive_tests,cumulative_positive_tests,reported_cases,cumulative_reported_cases,reported_deaths,cumulative_reported_deaths,reported_tests,cumulative_reported_tests
+
     # X Axis = Date converted to datetime
-    # Y Axis = Case count per 100K (newcountconfirmed / county population * 100,000)
+    # Y Axis = Case count per 100K (reported_cases / population * 100,000)
     data = [
             [
-                datetime.datetime.strptime(rows[5], "%Y-%m-%d"), 
-                (int(rows[3])/pop)*100000
-            ] 
-            for rows in cases_csv_reader 
-            if rows[0] == COUNTY
+                datetime.datetime.strptime(rows[0], "%Y-%m-%d"),
+                (int(float(rows[12])) / int(float(rows[3])))*100000
             ]
+            for rows in cases_csv_reader
+            if rows[0] and rows[1] == COUNTY
+           ]
 
 x = [row[0] for row in data]
 y = [row[1] for row in data]
